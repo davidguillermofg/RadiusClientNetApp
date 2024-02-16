4d5d276f8ab75a1587516d59bb26ab6128cc7c5c
BAUSTRO\ba0100067q <vguillermo@baustro.fin.ec>
2024-02-16 11:48:19 -0500

    initial

---
+++ RadiusClientNetApp/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace RadiusClientNetApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.txtSharedKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTimeout = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtAttribute = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "hostname:";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHostname.Location = new System.Drawing.Point(75, 32);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(345, 20);
+            this.txtHostname.TabIndex = 1;
+            this.txtHostname.Text = "10.16.32.48";
+            // 
+            // txtSharedKey
+            // 
+            this.txtSharedKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSharedKey.Location = new System.Drawing.Point(75, 59);
+            this.txtSharedKey.Name = "txtSharedKey";
+            this.txtSharedKey.Size = new System.Drawing.Size(345, 20);
+            this.txtSharedKey.TabIndex = 3;
+            this.txtSharedKey.Text = "12345678";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "sharedKey:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(75, 87);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(345, 20);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.Text = "vguillermo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(75, 114);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(345, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.Text = "vguillermo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "password:";
+            // 
+            // txtTimeout
+            // 
+            this.txtTimeout.Location = new System.Drawing.Point(74, 166);
+            this.txtTimeout.Name = "txtTimeout";
+            this.txtTimeout.Size = new System.Drawing.Size(78, 20);
+            this.txtTimeout.TabIndex = 9;
+            this.txtTimeout.Text = "5000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "timeout (ms):";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(74, 192);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(78, 20);
+            this.txtPort.TabIndex = 11;
+            this.txtPort.Text = "1812";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "port:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(8, 265);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(411, 79);
+            this.txtOutput.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Output:";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(74, 218);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(78, 23);
+            this.btnTest.TabIndex = 14;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtAttribute
+            // 
+            this.txtAttribute.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAttribute.Location = new System.Drawing.Point(75, 140);
+            this.txtAttribute.Name = "txtAttribute";
+            this.txtAttribute.Size = new System.Drawing.Size(345, 20);
+            this.txtAttribute.TabIndex = 16;
+            this.txtAttribute.Text = "Testing";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 143);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "attribute:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 350);
+            this.Controls.Add(this.txtAttribute);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTimeout);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSharedKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHostname);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(300, 350);
+            this.Name = "Form1";
+            this.Text = "RadiusClient .Net";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.TextBox txtSharedKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTimeout;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtAttribute;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ RadiusClientNetApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FP.Radius;
+
+namespace RadiusClientNetApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnTest_Click(object sender, EventArgs e)
+        {
+            string hostname = txtHostname.Text;
+            string sharedKey = txtSharedKey.Text;
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string attribute = txtAttribute.Text;
+            int timeout = Convert.ToInt16(txtTimeout.Text);
+            uint port = Convert.ToUInt16(txtPort.Text);
+            string output = "";
+
+            RadiusClient rc = new RadiusClient(hostname, sharedKey, timeout, port);
+            RadiusPacket authPacket = rc.Authenticate(username, password);
+            authPacket.SetAttribute(new VendorSpecificAttribute(10, 1, UTF8Encoding.UTF8.GetBytes(attribute)));
+            authPacket.SetAttribute(new VendorSpecificAttribute(10, 2, new[] { (byte)7 }));
+            RadiusPacket receivedPacket = await rc.SendAndReceivePacket(authPacket);
+
+            if (receivedPacket == null)
+            {
+                string errMsg = "Can't contact remote radius server ! \n";
+                output += errMsg;
+                MessageBox.Show(errMsg);
+                return;
+            }
+            output += "receivedPacket: " + receivedPacket.PacketType+" \n";
+            switch (receivedPacket.PacketType)
+            {
+                case RadiusCode.ACCESS_ACCEPT:
+                    output += "Access-Accept";
+                    foreach (var attr in receivedPacket.Attributes)
+                    {
+                        output += attr.Type.ToString() + " = " + attr.Value;
+                    }
+                    break;
+                case RadiusCode.ACCESS_CHALLENGE:
+                    output += "Access-Challenge";
+                    break;
+                case RadiusCode.ACCESS_REJECT:
+                    output += "Access-Reject";
+                    if (!rc.VerifyAuthenticator(authPacket, receivedPacket))
+                        output += "Authenticator check failed: Check your secret";
+                    break;
+                default:
+                    output += "Rejected";
+                    break;
+            }
+            output += " \n";
+            txtOutput.Text = output;
+        }
+    }
+}
